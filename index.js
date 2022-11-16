4cd3e400bf61f7068c444f075594d32d16add9d4
aparus <rustamapaev@gmail.com>
2022-11-16 14:50:59 +0400

    add js

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-export { AttendanceCreateDto, AttendanceUpdateDto } from './dto/attendances'
-export { CompanyCreateDto, CompanyUpdateDto } from './dto/companies'
-export { ContractCreateDto, ContractUpdateDto } from './dto/contracts'
+export { AttendanceCreateDto, AttendanceUpdateDto } from './dto/attendances.js'
+export { CompanyCreateDto, CompanyUpdateDto } from './dto/companies.js'
+export { ContractCreateDto, ContractUpdateDto } from './dto/contracts.js'
 export {
 	CounterpartyCompanyCreateDto,
 	CounterpartyCompanyUpdateDto
-} from './dto/counterparty-companies'
+} from './dto/counterparty-companies.js'
 export {
 	CounterpartyJurpersonCreateDto,
 	CounterpartyJurpersonUpdateDto
-} from './dto/counterparty-jurpersons'
+} from './dto/counterparty-jurpersons.js'
 export {
 	CounterpartyUnitCreateDto,
 	CounterpartyUnitUpdateDto
-} from './dto/counterparty-units'
-export { JurpersonCreateDto, JurpersonUpdateDto } from './dto/jurpersons'
-export { PositionCreateDto, PositionUpdateDto } from './dto/positions'
-export { ProfessionCreateDto, ProfessionUpdateDto } from './dto/professions'
+} from './dto/counterparty-units.js'
+export { JurpersonCreateDto, JurpersonUpdateDto } from './dto/jurpersons.js'
+export { PositionCreateDto, PositionUpdateDto } from './dto/positions.js'
+export { ProfessionCreateDto, ProfessionUpdateDto } from './dto/professions.js'
 export {
 	RedemptionTypeCreateDto,
 	RedemptionTypeUpdateDto
-} from './dto/redemption-types'
-export { RedemptionCreateDto, RedemptionUpdateDto } from './dto/redemptions'
-export { RoleCreateDto, RoleUpdateDto } from './dto/roles'
-export { UnitCreateDto, UnitUpdateDto } from './dto/units'
-export { UserCreateDto, UserUpdateDto } from './dto/users'
-export { WorkMeasureCreateDto, WorkMeasureUpdateDto } from './dto/work-measures'
-export { WorkParamCreateDto, WorkParamUpdateDto } from './dto/work-params'
-export { WorkerCreateDto, WorkerUpdateDto } from './dto/workers'
+} from './dto/redemption-types.js'
+export { RedemptionCreateDto, RedemptionUpdateDto } from './dto/redemptions.js'
+export { RoleCreateDto, RoleUpdateDto } from './dto/roles.js'
+export { UnitCreateDto, UnitUpdateDto } from './dto/units.js'
+export { UserCreateDto, UserUpdateDto } from './dto/users.js'
+export {
+	WorkMeasureCreateDto,
+	WorkMeasureUpdateDto
+} from './dto/work-measures.js'
+export { WorkParamCreateDto, WorkParamUpdateDto } from './dto/work-params.js'
+export { WorkerCreateDto, WorkerUpdateDto } from './dto/workers.js'
