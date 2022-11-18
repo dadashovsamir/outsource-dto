b3c071d5a11a88abd7d7b9ca3a1afa456e0a1bb8
aparus <rustamapaev@gmail.com>
2022-11-18 17:57:51 +0400

    add imports (bug)

--- jurpersons.ts
+++ jurpersons.ts
@@ -1,16 +1,16 @@
-import { PartialType } from '@nestjs/mapped-types'
-import { CompanyUpdateDto } from './companies'
+import { PartialType } from '@nestjs/mapped-types';
+import { CompanyReadDto, CompanyUpdateDto } from './companies';
 
 export class JurpersonCreateDto {
-	name: string
-	address: string
-	bankDetails: string
-	company: CompanyUpdateDto
+  name: string;
+  address: string;
+  bankDetails: string;
+  company: CompanyUpdateDto;
 }
 
 export class JurpersonUpdateDto extends PartialType(JurpersonCreateDto) {}
 
 export class JurpersonReadDto extends JurpersonCreateDto {
-	id: number
-	company: CompanyReadDto
+  id: number;
+  company: CompanyReadDto;
 }
--- units.ts
+++ units.ts
@@ -1,15 +1,15 @@
-import { PartialType } from '@nestjs/mapped-types'
-import { CompanyUpdateDto } from './companies'
+import { PartialType } from '@nestjs/mapped-types';
+import { CompanyReadDto, CompanyUpdateDto } from './companies';
 
 export class UnitCreateDto {
-	name: string
-	address: string
-	company: CompanyUpdateDto
+  name: string;
+  address: string;
+  company: CompanyUpdateDto;
 }
 
 export class UnitUpdateDto extends PartialType(UnitCreateDto) {}
 
 export class UnitReadDto extends UnitCreateDto {
-	id: number
-	company: CompanyReadDto
+  id: number;
+  company: CompanyReadDto;
 }
