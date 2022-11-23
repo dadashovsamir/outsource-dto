67e45393d22c67046ed79fea534ea941e5173a25
aparus <rustamapaev@gmail.com>
2022-11-23 22:59:47 +0400

    add jurperson constructor (for test)

--- jurpersons.ts
+++ jurpersons.ts
@@ -1,16 +1,19 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CompanyReadDto, CompanyUpdateDto } from './companies';
+import { PartialType } from '@nestjs/mapped-types'
+import { CompanyReadDto, CompanyUpdateDto } from './companies'
 
 export class JurpersonCreateDto {
-  name: string;
-  address: string;
-  bankDetails: string;
-  company: CompanyUpdateDto;
+	name: string = ''
+	address: string = ''
+	bankDetails: string = ''
+	company: CompanyUpdateDto
+	constructor(company: CompanyUpdateDto) {
+		this.company = company
+	}
 }
 
 export class JurpersonUpdateDto extends PartialType(JurpersonCreateDto) {}
 
 export class JurpersonReadDto extends JurpersonCreateDto {
-  id: number;
-  company: CompanyReadDto;
+	id: number
+	company: CompanyReadDto
 }
