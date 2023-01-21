62f87c45fb7dbc9cb33e7d2fc76831e44e1ba692
aparus <rustamapaev@gmail.com>
2023-01-21 10:51:55 +0400

    add comment field

--- payments-create.ts
+++ payments-create.ts
@@ -1,16 +1,16 @@
-import { CounterpartyUnitReadDto } from './counterparty-units-read'
-import { PositionReadDto } from './positions-read'
-import { PaymentTypeReadDto } from './payment-types-read'
-import { WorkerReadDto } from './workers-read'
+import { CounterpartyUnitReadDto } from './counterparty-units-read';
+import { PositionReadDto } from './positions-read';
+import { PaymentTypeReadDto } from './payment-types-read';
+import { WorkerReadDto } from './workers-read';
 
 export class PaymentCreateDto {
-	amount = 0
-	date = new Date()
-	period = new Date()
-	paymentType: PaymentTypeReadDto | { id: number } = { id: 0 }
-	worker: WorkerReadDto | { id: number } = { id: 0 }
-	//
-	payer: WorkerReadDto | { id: number } = { id: 0 }
-	position: PositionReadDto | { id: number } = { id: 0 }
-	counterpartyUnit: CounterpartyUnitReadDto | { id: number } = { id: 0 }
+  amount = 0;
+  date = new Date();
+  period = new Date();
+  paymentType: PaymentTypeReadDto | { id: number } = { id: 0 };
+  worker: WorkerReadDto | { id: number } = { id: 0 };
+  payer: WorkerReadDto | { id: number } = { id: 0 };
+  position: PositionReadDto | { id: number } = { id: 0 };
+  counterpartyUnit: CounterpartyUnitReadDto | { id: number } = { id: 0 };
+  comment = '';
 }
--- redemptions-create.ts
+++ redemptions-create.ts
@@ -1,14 +1,15 @@
-import { CounterpartyUnitReadDto } from './counterparty-units-read'
-import { PositionReadDto } from './positions-read'
-import { RedemptionTypeReadDto } from './redemption-types-read'
-import { WorkerReadDto } from './workers-read'
+import { CounterpartyUnitReadDto } from './counterparty-units-read';
+import { PositionReadDto } from './positions-read';
+import { RedemptionTypeReadDto } from './redemption-types-read';
+import { WorkerReadDto } from './workers-read';
 
 export class RedemptionCreateDto {
-	amount: number
-	date: Date
-	period: Date
-	redemptionType: RedemptionTypeReadDto | { id: number }
-	worker: WorkerReadDto | { id: number }
-	position: PositionReadDto | { id: number }
-	counterpartyUnit: CounterpartyUnitReadDto | { id: number }
+  amount: number;
+  date: Date;
+  period: Date;
+  redemptionType: RedemptionTypeReadDto | { id: number };
+  worker: WorkerReadDto | { id: number };
+  position: PositionReadDto | { id: number };
+  counterpartyUnit: CounterpartyUnitReadDto | { id: number };
+  comment = '';
 }
