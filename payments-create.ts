a9f0549108c467f42151de4bbe4f7a556fe12d48
aparus <rustamapaev@gmail.com>
2024-03-14 06:22:11 +0300

    payment field worker changed to recipient

--- payments-create.ts
+++ payments-create.ts
@@ -8,8 +8,8 @@ export class PaymentCreateDto {
   date = new Date();
   period = new Date();
   paymentType: PaymentTypeReadDto | { id: number } = { id: 0 };
-  worker: WorkerReadDto | { id: number } = { id: 0 };
   payer: WorkerReadDto | { id: number } = { id: 0 };
+  recipient: WorkerReadDto | { id: number } = { id: 0 };
   position: PositionReadDto | { id: number } = { id: 0 };
   counterpartyUnit: CounterpartyUnitReadDto | { id: number } = { id: 0 };
   comment = '';
--- payments-read.ts
+++ payments-read.ts
@@ -1,18 +1,18 @@
-import { CounterpartyUnitReadDto } from './counterparty-units-read'
-import { PositionReadDto } from './positions-read'
-import { PaymentTypeReadDto } from './payment-types-read'
-import { PaymentCreateDto } from './payments-create'
-import { WorkerReadDto } from './workers-read'
+import { CounterpartyUnitReadDto } from './counterparty-units-read';
+import { PositionReadDto } from './positions-read';
+import { PaymentTypeReadDto } from './payment-types-read';
+import { PaymentCreateDto } from './payments-create';
+import { WorkerReadDto } from './workers-read';
 
 export class PaymentReadDto extends PaymentCreateDto {
-	id = 0
-	paymentType: PaymentTypeReadDto
-	worker: WorkerReadDto
-	payer: WorkerReadDto
-	position: PositionReadDto
-	counterpartyUnit: CounterpartyUnitReadDto
-	constructor(worker: WorkerReadDto) {
-		super()
-		this.worker = worker
-	}
+  id = 0;
+  paymentType: PaymentTypeReadDto;
+  payer: WorkerReadDto;
+  recipient: WorkerReadDto;
+  position: PositionReadDto;
+  counterpartyUnit: CounterpartyUnitReadDto;
+  constructor(recipient: WorkerReadDto) {
+    super();
+    this.recipient = recipient;
+  }
 }
