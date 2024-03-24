985438fd4c7689eabb3f065490bbc4c2fb9d7df5
aparus <rustamapaev@gmail.com>
2024-03-24 11:22:25 +0300

    remove unnecessary

--- payments-create.ts
+++ payments-create.ts
@@ -1,16 +1,12 @@
-import { CounterpartyUnitReadDto } from './counterparty-units-read';
-import { PositionReadDto } from './positions-read';
-import { PaymentTypeReadDto } from './payment-types-read';
-import { WorkerReadDto } from './workers-read';
+import { PaymentTypeReadDto } from './payment-types-read'
+import { WorkerReadDto } from './workers-read'
 
 export class PaymentCreateDto {
-  amount = 0;
-  date = new Date();
-  period = new Date();
-  paymentType: PaymentTypeReadDto | { id: number } = { id: 0 };
-  payer: WorkerReadDto | { id: number } = { id: 0 };
-  recipient: WorkerReadDto | { id: number } = { id: 0 };
-  position: PositionReadDto | { id: number } = { id: 0 };
-  counterpartyUnit: CounterpartyUnitReadDto | { id: number } = { id: 0 };
-  comment = '';
+	amount = 0
+	date = new Date()
+	period = new Date()
+	paymentType: PaymentTypeReadDto | { id: number } = { id: 0 }
+	payer: WorkerReadDto | { id: number } = { id: 0 }
+	recipient: WorkerReadDto | { id: number } = { id: 0 }
+	comment = ''
 }
--- payments-read.ts
+++ payments-read.ts
@@ -1,18 +1,14 @@
-import { CounterpartyUnitReadDto } from './counterparty-units-read';
-import { PositionReadDto } from './positions-read';
-import { PaymentTypeReadDto } from './payment-types-read';
-import { PaymentCreateDto } from './payments-create';
-import { WorkerReadDto } from './workers-read';
+import { PaymentTypeReadDto } from './payment-types-read'
+import { PaymentCreateDto } from './payments-create'
+import { WorkerReadDto } from './workers-read'
 
 export class PaymentReadDto extends PaymentCreateDto {
-  id = 0;
-  paymentType: PaymentTypeReadDto;
-  payer: WorkerReadDto;
-  recipient: WorkerReadDto;
-  position: PositionReadDto;
-  counterpartyUnit: CounterpartyUnitReadDto;
-  constructor(recipient: WorkerReadDto) {
-    super();
-    this.recipient = recipient;
-  }
+	id = 0
+	paymentType: PaymentTypeReadDto
+	payer: WorkerReadDto
+	recipient: WorkerReadDto
+	constructor(initialData?: Partial<PaymentReadDto>) {
+		super()
+		Object.assign(this, initialData)
+	}
 }
