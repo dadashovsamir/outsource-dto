1c102adade14e46cdd37f9c2dfa45f19cf0e05ad
aparus <rustamapaev@gmail.com>
2024-08-02 17:58:02 +0300

    update redemption-filter

--- redemption-filter.ts
+++ redemption-filter.ts
@@ -2,7 +2,9 @@ import { CounterpartyCompanyReadDto } from './counterparty-companies-read'
 import { CounterpartyJurpersonReadDto } from './counterparty-jurpersons-read'
 import { CounterpartyUnitReadDto } from './counterparty-units-read'
 import { JurpersonReadDto } from './jurpersons-read'
+import { PricePositionReadDto } from './price-positions-read'
 import { RedemptionTypeReadDto } from './redemption-types-read'
+import { WorkParamReadDto } from './work-params-read'
 import { WorkerReadDto } from './workers-read'
 
 export class RedemptionFilterDto {
@@ -11,9 +13,11 @@ export class RedemptionFilterDto {
 	periodStart: string = ''
 	periodEnd: string = ''
 	worker: WorkerReadDto[] | { id: number }[] = []
+	workParamsWorker: WorkParamReadDto[] | { id: number }[] = []
 	counterpartyCompany: CounterpartyCompanyReadDto[] | { id: number }[] = []
 	counterpartyJurperson: CounterpartyJurpersonReadDto[] | { id: number }[] = []
 	counterpartyUnit: CounterpartyUnitReadDto[] | { id: number }[] = []
 	jurperson: JurpersonReadDto[] | { id: number }[] = []
 	redemptionType: RedemptionTypeReadDto[] | { id: number }[] = []
+	pricePosition: PricePositionReadDto[] = []
 }
