ab2411f1bd8e688414b90942174cc96c5d84120a
aparus <rustamapaev@gmail.com>
2024-08-15 15:03:59 +0300

    attendance-filter add unitPosition

--- attendance-filter.ts
+++ attendance-filter.ts
@@ -2,6 +2,7 @@ import { CounterpartyCompanyReadDto } from './counterparty-companies-read'
 import { CounterpartyJurpersonReadDto } from './counterparty-jurpersons-read'
 import { CounterpartyUnitReadDto } from './counterparty-units-read'
 import { JurpersonReadDto } from './jurpersons-read'
+import { UnitPositionReadDto } from './unit-positions-read'
 import { WorkerReadDto } from './workers-read'
 
 export class AttendanceFilterDto {
@@ -12,4 +13,5 @@ export class AttendanceFilterDto {
 	counterpartyJurperson: CounterpartyJurpersonReadDto[] | { id: number }[] = []
 	counterpartyUnit: CounterpartyUnitReadDto[] | { id: number }[] = []
 	jurperson: JurpersonReadDto[] | { id: number }[] = []
+	unitPosition: UnitPositionReadDto[] | { id: number }[] = []
 }
