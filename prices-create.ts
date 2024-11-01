406ae438823f69a2b71a0b8e67ceb44a9023fd76
aparus <rustamapaev@gmail.com>
2024-11-01 16:15:28 +0300

    add jurperson (our ) deleted before by mistake

--- prices-create.ts
+++ prices-create.ts
@@ -1,8 +1,10 @@
 import { CounterpartyUnitReadDto } from './counterparty-units-read'
+import { JurpersonReadDto } from './jurpersons-read'
 import { PriceGroupReadDto } from './price-groups-read'
 
 export class PriceCreateDto {
 	date: Date = new Date()
+	jurperson: JurpersonReadDto | { id: number } = { id: 0 }
 	counterpartyUnit?: CounterpartyUnitReadDto | { id: number }
 	priceGroup?: PriceGroupReadDto | { id: number }
 	comment = ''
--- prices-read.ts
+++ prices-read.ts
@@ -1,9 +1,11 @@
 import { PriceCreateDto } from './prices-create'
 import { CounterpartyUnitReadDto } from './counterparty-units-read'
 import { PriceGroupReadDto } from './price-groups-read'
+import { JurpersonReadDto } from './jurpersons-read'
 
 export class PriceReadDto extends PriceCreateDto {
 	id = 0
+	jurperson: JurpersonReadDto
 	counterpartyUnit?: CounterpartyUnitReadDto
 	priceGroup?: PriceGroupReadDto
 	constructor(initialData?: Partial<PriceReadDto>) {
